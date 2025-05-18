cbb46716f31884b43c7a21d794eb798f4d0cfd8a
gianmarcoannese92 <gianmarcoannese@gmail.com>
2025-05-18 16:07:19 +0200

    Quartz sync: May 18, 2025, 4:07 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,11 +7,9 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <a href="https://mangiarco.github.io/digit-garden">
       <h2 class={classNames(displayClass, "page-title")} align="center">
         <img src='https://i.ibb.co/HLgwd8B7/swan.png' style={{ width: "45%" }}/>
       </h2>
-    </a>
   )
 }
 
