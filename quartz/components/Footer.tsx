0c00541bf9671b5084f8d6ff4bca825d85230fe5
gianmarcoannese92 <gianmarcoannese@gmail.com>
2025-05-22 12:15:01 +0200

    Quartz sync: May 22, 2025, 12:15 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -6,20 +6,26 @@ import { i18n } from "../i18n"
 interface Options {
   links: Record<string, string>
 }
-
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p align="center">
-          <a href="https://mangiarco.github.io/digit-garden/licence"><img src="https://img.shields.io/badge/License-CC_BY_4.0-blue.svg?style=flat" /></a>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href="https://mangiarco.github.io/digit-garden/licence"><img src="https://img.shields.io/badge/License-CC_BY_4.0-blue.svg?style=flat" /></a>
+            </li>
+          ))}
+        </ul>
       </footer>
     )
   }
-
   Footer.css = style
   return Footer
 }) satisfies QuartzComponentConstructor
